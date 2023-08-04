c84537fdbe224032bf508c63446b3a2d0debbd8c
Felipe Ulloa <felipeulloa5a43@gmail.com>
2023-08-04 15:03:49 -0400

    feat: :sparkles: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,162 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-export default function Home() {
+const TravelForm = () => {
+  const [numPasajeros, setNumPasajeros] = useState(1);
+  const [pasajeros, setPasajeros] = useState([
+    { nombre: "", fono: "", domicilio: "" },
+  ]);
+  const [fecha, setFecha] = useState("");
+  const [horaLlegada, setHoraLlegada] = useState("");
+  const [dirInicio, setDirInicio] = useState("");
+  const [dirDestino, setDirDestino] = useState("");
+
+  useEffect(() => {
+    setPasajeros(
+      [...Array(numPasajeros)].map(() => ({
+        nombre: "",
+        fono: "",
+        domicilio: "",
+      }))
+    );
+  }, [numPasajeros]);
+
+  const onSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    const data = {
+      fecha,
+      hora_llegada: horaLlegada,
+      dir_inicio: dirInicio,
+      dir_destino: dirDestino,
+      pasajeros,
+    };
+
+    axios
+      .post("http://localhost:3001/viajes", data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Container>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form onSubmit={onSubmit}>
+            <Form.Group controlId="date">
+              <Form.Label>Fecha</Form.Label>
+              <Form.Control
+                type="date"
+                value={fecha}
+                onChange={(e) => setFecha(e.target.value)}
+                required
+              />
+            </Form.Group>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Form.Group controlId="arrivalTime">
+              <Form.Label>Hora de Llegada</Form.Label>
+              <Form.Control
+                type="time"
+                value={horaLlegada}
+                onChange={(e) => setHoraLlegada(e.target.value)}
+                required
+              />
+            </Form.Group>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <Form.Group controlId="startAddress">
+              <Form.Label>Dirección de inicio</Form.Label>
+              <Form.Control
+                type="text"
+                value={dirInicio}
+                onChange={(e) => setDirInicio(e.target.value)}
+                required
+              />
+            </Form.Group>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <Form.Group controlId="endAddress">
+              <Form.Label>Dirección de destino</Form.Label>
+              <Form.Control
+                type="text"
+                value={dirDestino}
+                onChange={(e) => setDirDestino(e.target.value)}
+                required
+              />
+            </Form.Group>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+            <Form.Group controlId="numPasajeros">
+              <Form.Label>Número de Pasajeros</Form.Label>
+              <Form.Control
+                as="select"
+                value={numPasajeros}
+                onChange={(e) => setNumPasajeros(Number(e.target.value))}
+                required
+              >
+                {[1, 2, 3, 4].map((val, idx) => (
+                  <option key={idx} value={val}>
+                    {val}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+            {pasajeros.map((pasajero, index) => (
+              <div key={index}>
+                <Form.Group controlId={`pasajeros[${index}].nombre`}>
+                  <Form.Label>Nombre Pasajero {index + 1}</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={pasajero.nombre}
+                    onChange={(e) => {
+                      const newPasajeros = [...pasajeros];
+                      newPasajeros[index].nombre = e.target.value;
+                      setPasajeros(newPasajeros);
+                    }}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId={`pasajeros[${index}].fono`}>
+                  <Form.Label>Telefono Pasajero {index + 1}</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={pasajero.fono}
+                    onChange={(e) => {
+                      const newPasajeros = [...pasajeros];
+                      newPasajeros[index].fono = e.target.value;
+                      setPasajeros(newPasajeros);
+                    }}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId={`pasajeros[${index}].domicilio`}>
+                  <Form.Label>Domicilio Pasajero {index + 1}</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={pasajero.domicilio}
+                    onChange={(e) => {
+                      const newPasajeros = [...pasajeros];
+                      newPasajeros[index].domicilio = e.target.value;
+                      setPasajeros(newPasajeros);
+                    }}
+                    required
+                  />
+                </Form.Group>
+              </div>
+            ))}
+
+            <Button variant="primary" type="submit">
+              Enviar
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default TravelForm;
